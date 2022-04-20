ea270b2dd2370a7ed3383144ea1a01c904591b67
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-21 03:16:59 +0600

    gitignore

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/',(req,res) =>{
     res.send('Look mama I can code on node and I feel good')
@@ -17,7 +21,16 @@ const users= [
 ]
 
 app.get('/users',(req,res) =>{
-    res.send(users)
+    console.log('query',req.query);
+    if(req.query.name){
+      const search = req.query.name.toLowerCase();
+      const matched = users.filter(user =>user.name.toLowerCase().includes(search))
+      res.send(matched);
+    }
+    else{
+        res.send(users)
+    }
+    
 })
 
 app.get('/user/:id', (req,res)=>{
@@ -25,6 +38,14 @@ app.get('/user/:id', (req,res)=>{
    const id = parseInt(req.params.id);
    const user = users.find(u => u.id == id);
    res.send(user)
+})
+
+app.post('/user',(req,res)=>{
+    console.log('request',req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
 })
 
 app.listen(port,() =>{
